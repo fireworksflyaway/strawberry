31e91307b29cc21e4a12c3455177401a62b07d58
fireworksflyaway <fireworksflyaway@live.com>
2017-11-22 14:06:37 +0800

    install sass

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import {Button} from 'antd';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
