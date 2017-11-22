a74ac64e783011aae677d9bf0c9deeb5db181bb1
fireworksflyaway <fireworksflyaway@live.com>
2017-11-22 16:20:21 +0800

    layout and logo

--- src/page/App.js
+++ src/page/App.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
+import logo from '../image/logo.svg';
+import {BrowserRouter, Link} from 'react-router-dom';
+import {Layout} from 'antd';
 import '../style/App.scss';
 
+
+const {Header, Content,  Footer} = Layout;
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        Hello
-      </div>
-    );
-  }
+        render(){
+                return(
+                        <div>
+                                <BrowserRouter>
+                                        <Layout>
+                                                <Header className='appHeader'>
+                                                        <Link to='/'><img src={logo} alt="Brainnow"  className='logoImg'/></Link>
+                                                </Header>
+                                                <Content>
+                                                        Content
+                                                </Content>
+                                                <Footer>Footer</Footer>
+                                        </Layout>
+                                </BrowserRouter>
+                        </div>
+                );
+        }
 }
 
 export default App;
