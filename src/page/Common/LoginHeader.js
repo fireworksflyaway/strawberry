b5a0196970ac72f7647e26311f2dda4ff99771a8
mason <fireworksflyaway@live.com>
2018-04-03 10:14:43 +0800

    Web 0.1.3 dev 1 1. 修复导航栏蓝色条刷新不显示的bug

--- src/page/Common/LoginHeader.js
+++ src/page/Common/LoginHeader.js
@@ -6,6 +6,7 @@ import { Menu,  Button , Icon} from 'antd';
 import {Link} from 'react-router-dom';
 export default class LoginHeader extends React.Component{
         render(){
+                console.log(this.props.selectKey);
                 return (
                     <Menu
                         className='headerMenu'
@@ -13,11 +14,11 @@ export default class LoginHeader extends React.Component{
                         defaultSelectedKeys={[this.props.selectKey]}
                     >
                             <Menu.Item key="/"><Link to='/' >首页</Link></Menu.Item>
-                            <Menu.Item key={`/${this.props.type}upload`}><Link to={`/${this.props.type}Upload`}>上传数据</Link></Menu.Item>
-                            <Menu.Item key={`/${this.props.type}event`}><Link to={`/${this.props.type}Event`}>查看任务进度</Link></Menu.Item>
-                            <Menu.Item key={`/${this.props.type}report`}><Link to={`/${this.props.type}Report`} >查看报告</Link></Menu.Item>
+                            <Menu.Item key={`/${this.props.type}Upload`}><Link to={`/${this.props.type}Upload`}>上传数据</Link></Menu.Item>
+                            <Menu.Item key={`/${this.props.type}Event`}><Link to={`/${this.props.type}Event`}>查看任务进度</Link></Menu.Item>
+                            <Menu.Item key={`/${this.props.type}Report`}><Link to={`/${this.props.type}Report`} >查看报告</Link></Menu.Item>
                             <Menu.SubMenu title={<strong><Icon type="user" />{this.props.username}</strong>}>
-                                    <Menu.Item key={`${this.props.type}profile`}><Link to={`/Profile`} ><Icon type="edit" />编辑个人信息</Link></Menu.Item>
+                                    <Menu.Item key={`${this.props.type}Profile`}><Link to={`/Profile`} ><Icon type="edit" />编辑个人信息</Link></Menu.Item>
                                     <Menu.Item key="user:2"><Link to='/' onClick={this.props.handleLogout}><Icon type="logout"/>注销</Link></Menu.Item>
                             </Menu.SubMenu>
                             <Button style={{marginLeft:'40px'}}>EN</Button>
