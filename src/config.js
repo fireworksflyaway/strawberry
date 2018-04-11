88d9a70cbd90b3f35c9888dc97dd89acc1f0f64d
mason <fireworksflyaway@live.com>
2018-04-11 14:37:52 +0800

    commit log info

--- src/config.js
+++ src/config.js
@@ -3,13 +3,13 @@
  */
 
 export default {
-        //ubuntu
-        server: "http://localhost:8090",
-        fileServer:"http://localhost:3000/frontData"
+        // //ubuntu
+        // server: "http://localhost:8090",
+        // fileServer:"http://localhost:3000/frontData"
 
-        // //Real Server
-        // server: "http://192.168.0.104:8090",
-        // fileServer:"http://192.168.0.104/frontData"
+        //Real Server
+        server: "http://192.168.0.104:8090",
+        fileServer:"http://192.168.0.104/frontData"
 
         // //Real IP Server
         // server: "http://183.47.50.121:8080",
